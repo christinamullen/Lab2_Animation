1c09b055163b5b9db0f96864489639b32d248381
Christina dt <cnicodc@gmail.com>
2023-09-07 12:40:40 -0700

    formatting clarity

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@ const button = document.getElementById('bookButton');
 const pond = button.querySelector('.pond');
 const text = button.querySelector('.button_text');
 
-button.addEventListener('click', function() {
+button.addEventListener('click', function () {
     //hide text
     text.style.visibility = 'hidden';
 
     //reset css animations for each circle so no repeat
     Array.from(pond.children).forEach(circle => {
-        circle.style.animation = 'none';  
+        circle.style.animation = 'none';
         circle.offsetHeight;
     });
 
@@ -16,17 +16,17 @@ button.addEventListener('click', function() {
     pond.querySelector('.in').style.animation = 'showCircle 0.5s 1s forwards, hideCircle 0.5s 3s forwards';
     pond.querySelector('.mid').style.animation = 'showCircle 0.5s 1.5s forwards, hideCircle 0.5s 2.5s forwards';
     pond.querySelector('.out').style.animation = 'showCircle 0.5s 2s forwards, hideCircle 0.5s 2s forwards';
-    
+
     //change text to booking
-    setTimeout(function() {
+    setTimeout(function () {
         text.style.visibility = 'visible';
         text.textContent = 'Booking...';
 
         //wait 3 seconds to show completed
-        setTimeout(function() {
+        setTimeout(function () {
             text.textContent = 'You\'re Booked!';
         }, 3000);
 
     }, 4000);
-    
+
 });
