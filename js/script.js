bb914c8ad3e8175778cb3c76e3c2059a810506b3
Christina dt <cnicodc@gmail.com>
2023-09-06 17:45:00 -0700

    dots meh

--- js/script.js
+++ js/script.js
@@ -3,14 +3,19 @@ const dots = button.querySelector('.dots');
 const text = button.querySelector('.button_text');
 
 button.addEventListener('click', function() {
-    
+    //dots visible
     dots.style.visibility = 'visible';
     text.style.visibility = 'hidden';
-    setTimeout(function() {
-        text.style.visibility = 'visible';
-        text.textContent = 'loading...';
+    dots.style.right='40px';
+    Array.from(dots.children).forEach(dot =>{
+        dot.style.opacity = '1';
+    });
+    //text.style.visibility = 'visible';
+    text.textContent = 'loading...';
 
+    setTimeout(function() {
+        dots.style.right = '-100px'
         dots.style.visibility = 'hidden';
-        label.textContent = 'Book Now';
+        text.textContent = 'Book Now';
     }, 4000);
 });
