53e2d42ac289416d2fc9536ebef7ea7f951879bc
Christina dt <cnicodc@gmail.com>
2023-09-07 11:51:08 -0700

    animation workaround

--- js/script.js
+++ js/script.js
@@ -3,23 +3,30 @@ const pond = button.querySelector('.pond');
 const text = button.querySelector('.button_text');
 
 button.addEventListener('click', function() {
-    //dots visible
+    //hide text
     text.style.visibility = 'hidden';
 
+    //reset css animations for each circle so no repeat
     Array.from(pond.children).forEach(circle => {
         circle.style.animation = 'none';  
         circle.offsetHeight;
     });
 
+    //pond ripples in mid out
     pond.querySelector('.in').style.animation = 'showCircle 0.5s 1s forwards, hideCircle 0.5s 3s forwards';
     pond.querySelector('.mid').style.animation = 'showCircle 0.5s 1.5s forwards, hideCircle 0.5s 2.5s forwards';
     pond.querySelector('.out').style.animation = 'showCircle 0.5s 2s forwards, hideCircle 0.5s 2s forwards';
-
     
-
+    //change text to booking
     setTimeout(function() {
         text.style.visibility = 'visible';
         text.textContent = 'Booking...';
-        //text.textContent = 'Book Now';
+
+        //wait 3 seconds to show completed
+        setTimeout(function() {
+            text.textContent = 'You\'re Booked!';
+        }, 3000);
+
     }, 4000);
+    
 });
